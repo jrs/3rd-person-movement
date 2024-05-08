13778fc04aa8c9e4335a5fe345ddada4a5d3208e
Justin Scott <hello@jrs.dev>
2024-05-08 11:22:05 -0700

    Create EnemyMovement.cs

---
+++ EnemyMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed = 1f;
+    public float rangeValue = 5f;
+    [SerializeField] private Transform _player;
+    [SerializeField] private Animator _enemyAnimation;
+    [SerializeField] private Rigidbody _enemyRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movementDirection = (_player.position - transform.position).normalized;
+        
+        float distance = Vector3.Distance(_player.position, transform.position);
+        Debug.Log("Distance " + distance);
+        if(distance < rangeValue)
+        {
+            _enemyRb.velocity = movementDirection * speed;
+            transform.LookAt(_player);
+            _enemyAnimation.SetBool("IsMoving", true);
+        }
+        else
+        {
+            _enemyAnimation.SetBool("IsMoving", false);
+            _enemyRb.velocity = movementDirection * 0;
+        }
+    }// end Update
+}
