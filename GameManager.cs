05d54d86af6c0edd7e13dba823dab62f819b0dae
Justin Scott <hello@jrs.dev>
2024-05-20 11:06:21 -0700

    Update GameManager.cs

--- GameManager.cs
+++ GameManager.cs
@@ -6,7 +6,8 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set;}
 
-    [SerializeField] private int currentLevelNumber = 1;
+    [SerializeField] private int _currentLevelNumber = 1;
+    [SerializeField] private bool _levelOneCompleted, _levelTwoCompleted, _levelThreeCompleted, _levelFourCompleted, _gameIsCompleted;
 
     void Awake()
     {
@@ -24,6 +25,50 @@ public class GameManager : MonoBehaviour
     
     public int GetCurrentLevelNumber()
     {
-        return currentLevelNumber;
+        return _currentLevelNumber;
+    }
+
+    public void CompletedLevel(string levelName)
+    {
+        switch (levelName)
+        {
+            case "Level 1": _levelOneCompleted = true;
+            break;
+            case "Level 2": _levelTwoCompleted = true;
+            break;
+            case "Level 3": _levelThreeCompleted = true; 
+            break;
+            case "Level 4": _levelFourCompleted = true;
+            break; 
+            default: _gameIsCompleted = true;
+            break;
+        }
+
+        if(_levelOneCompleted)
+        {
+            _currentLevelNumber = 2;
+        }
+        if(_levelTwoCompleted)
+        {
+            _currentLevelNumber = 3;
+        }
+        if(_levelThreeCompleted)
+        {
+            _currentLevelNumber = 4;
+        }
+        if(_levelFourCompleted)
+        {
+            ResetGame();
+        }
+    }
+
+    public void ResetGame()
+    {
+        _currentLevelNumber = 1;
+        _levelOneCompleted = false;
+        _levelTwoCompleted = false;
+        _levelThreeCompleted = false;
+        _levelFourCompleted = false;
+        _gameIsCompleted = false;
     }
 }
